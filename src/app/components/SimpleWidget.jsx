b0bc9b053d8f54744ef07fbdf9fd6672b4dc8ffc
yovito111 <giovicr111@gmail.com>
2024-08-21 16:17:11 -0400

    save changes

--- src/app/components/SimpleWidget.jsx
+++ src/app/components/SimpleWidget.jsx
@@ -6,7 +6,7 @@ export const SimpleWidget = () => {
     <div className="bg-white shadow-xl p-3 sm:min-w-[25%] min-w-full  rounded-2xl border-1 border-gray-50 mx-2">
       <div className="flex flex-col">
         <div>
-          <h2 className="font-bold text-gray-600 text-center">Contador</h2>
+          <h2 className="font-bold text-gray-600 text-center">Hola si</h2>
         </div>
         <div className="my-3">
           <div className="flex flex-row items-center justify-center space-x-1 ">
